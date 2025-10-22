64c6e60e65251bb3c0574ad8e940a601c58482f7
Amos Barbato <amosbarbato@gmail.com>
2025-10-21 21:41:19 -0300

    feat: created badge component

---
+++ components/ui/badge.tsx
@@ -0,0 +1,29 @@
+import type { ComponentProps } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const badgeVariants = cva(
+  "inline-block rounded-md text-xs uppercase px-3 py-1.5 w-fit",
+  {
+    variants: {
+      variant: {
+        primary: "bg-blue-400 text-blue-200",
+        secondary: "bg-cyan-300 text-cyan-100",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  },
+);
+
+interface BadgeProps
+  extends VariantProps<typeof badgeVariants>,
+    ComponentProps<"div"> {}
+
+export default function Badge({ className, children, ...props }: BadgeProps) {
+  return (
+    <div className={badgeVariants({ className })} {...props}>
+      <span className="font-semibold">{children}</span>
+    </div>
+  );
+}
