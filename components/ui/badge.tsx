9fa7110bf4324dbc62a1d7c54adab685244c4372
Amos Barbato <amosbarbato@gmail.com>
2025-10-23 12:21:55 -0300

    fix: added variant color to badge component

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps } from "react";
-import { cva, type VariantProps } from "class-variance-authority";
+import { cva, cx, type VariantProps } from "class-variance-authority";
 
 export const badgeVariants = cva(
   "inline-block rounded-md text-xs uppercase px-3 py-1.5 w-fit",
@@ -20,9 +20,14 @@ interface BadgeProps
   extends VariantProps<typeof badgeVariants>,
     ComponentProps<"div"> {}
 
-export default function Badge({ className, children, ...props }: BadgeProps) {
+export default function Badge({
+  className,
+  variant,
+  children,
+  ...props
+}: BadgeProps) {
   return (
-    <div className={badgeVariants({ className })} {...props}>
+    <div className={cx(badgeVariants({ variant }), className)} {...props}>
       <span className="font-semibold">{children}</span>
     </div>
   );
