8ce34bd5fb762cc5552f6864740ab9a271b48cb6
Amos Barbato <amosbarbato@gmail.com>
2025-10-23 15:26:06 -0300

    feat: added post dynamic list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,11 @@
 import Search from "@/components/search";
 import Badge from "@/components/ui/badge";
 import PostCard from "./components/post-card";
+import { allPosts } from "@/.contentlayer/generated";
 
 export default function Blog() {
+  const posts = allPosts;
+
   return (
     <article>
       <section className="padding-container space-y-14 pt-20 pb-[136px]">
@@ -22,40 +25,20 @@ export default function Blog() {
 
         {/* list */}
         <div className="grid gap-4 md:grid-cols-3 md:gap-6">
-          <PostCard
-            title="Transformando seu negócio em uma loja virtual"
-            description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-            date="20/12/2024"
-            slug="transformando"
-            image="/assets/primeiro-post.png"
-            author={{
-              name: "Aspen Dokidis",
-              avatar: "/customer-01.png",
-            }}
-          />
-          <PostCard
-            title="5 motivos para escolher o Site.Set"
-            description="O Site.Set oferece tudo o que você precisa para criar uma loja online eficiente e personalizada. Desde a integração com sistemas de pagamento até a criação de páginas de vendas, veja por que milhares de empreendedores escolhem nossa plataforma."
-            date="26/11/2024"
-            slug="transformando"
-            image="/assets/segundo-post.png"
-            author={{
-              name: "Dulce Westervelt",
-              avatar: "/customer-03.png",
-            }}
-          />
-
-          <PostCard
-            title="Como atrair consumidores"
-            description="Atrair consumidores para sua loja online é uma das chaves para o sucesso no e-commerce. Para conquistar novos clientes, é essencial adotar estratégias eficazes de marketing e criar uma experiência de compra que os faça voltar. Aqui estão algumas dicas para aumentar o tráfego e a conversão na sua loja:"
-            date="24/09/2024"
-            slug="transformando"
-            image="/assets/terceiro-post.png"
-            author={{
-              name: "Aspen Dokidis",
-              avatar: "/customer-01.png",
-            }}
-          />
+          {posts.map((post) => (
+            <PostCard
+              key={post._id}
+              title={post.title}
+              description={post.description}
+              date={post.date}
+              slug={post.slug}
+              image={post.image}
+              author={{
+                name: post.author.name,
+                avatar: post.author.avatar,
+              }}
+            />
+          ))}
         </div>
       </section>
     </article>
